dd59f217adf0bb20bd399ecfec783b063174d6bb
cookiesikeri <ikbenezer@gmail.com>
2023-02-10 08:54:09 +0100

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const multer = require("multer");
 const mysql = require("mysql2");
 const path = require("path");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,34 +15,34 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // config multers.
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public");
-  },
-  filename: function (req, file, cb) {
-    const filename = file.mimetype.includes('image') ? `${file.fieldname}-${Date.now()}.jpg` : `${file.fieldname}-${Date.now()}.mp4`
-    cb(null, filename);
-  },
+    destination: function(req, file, cb) {
+        cb(null, "public");
+    },
+    filename: function(req, file, cb) {
+        const filename = file.mimetype.includes('image') ? `${file.fieldname}-${Date.now()}.jpg` : `${file.fieldname}-${Date.now()}.mp4`
+        cb(null, filename);
+    },
 });
 
 const upload = multer({ storage: storage });
 
 // create datbase connection
 const dbConn = mysql.createConnection({
-  host: process.env.DB_HOST || "",
-  user: process.env.DB_USER_NAME || "",
-  password: process.env.DB_USER_PASSWORD || "",
-  database: process.env.DB_NAME || "",
-  port: process.env.DB_PORT || "",
+    host: process.env.DB_HOST || "",
+    user: process.env.DB_USER_NAME || "",
+    password: process.env.DB_USER_PASSWORD || "",
+    database: process.env.DB_NAME || "",
+    port: process.env.DB_PORT || "",
 });
 
-dbConn.connect(function (err) {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
-  console.log("Database was connected");
-  require("./routes")({ app, dbConn, upload });
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+dbConn.connect(function(err) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log("Database was connected");
+    require("./routes")({ app, dbConn, upload });
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
 });
